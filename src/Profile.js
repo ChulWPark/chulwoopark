95957f665b93ecd885d8ab62563c6246dafa617d
Chul Woo Park <cwparknj@gmail.com>
2022-09-08 22:00:42 -0700

    Update Profile.js

--- src/Profile.js
+++ src/Profile.js
@@ -26,11 +26,11 @@ export const Profile = (props) => (
         <FormText>
             <h2>Chul Woo Park</h2>
             <hr width="40%"></hr>
-            <p>Software Developer at Epic</p>
+            <p>Software Engineer at Google</p>
             <p>BS Computer Engineering '19 at Purdue University</p>
             <hr width="80%"></hr>
-            <p>Software: Python, C, C++, Java, <u>C#</u>, <u>JavaScript</u>, <u>HTML</u>, <u>CSS</u>, <u>VB</u></p>
-            <p>Database: SQL, <u>M (MUMPS - NoSQL for healthcare industry)</u></p>
+            <p>Software: <u>Java</u>, <u>Go</u>, Python, C, C++, C#, JavaScript, HTML, CSS, VB</p>
+            <p>Database: SQL, M (MUMPS - NoSQL for healthcare industry)</p>
             <p>Hardware: Verilog, Assembly (STM32), ABEL, LT Spice</p>
             <p>Languages: <u>English</u>, <u>Korean</u>, Spanish</p>
             <br/><br/><br/>
